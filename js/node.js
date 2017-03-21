50bb90caa795bf2c914f71d4927f245b81705e6b
yurajun <yurajun@mail.ru>
2017-03-21 13:45:07 +0300

    final searc

--- js/node.js
+++ js/node.js
@@ -2,11 +2,13 @@ let ARTICLES = [
 	{
 		id: 1,
 		author: 'Yura Jun',
-		article: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores perferendis hic deserunt cumque reiciendis distinctio repellat doloremque in, quas, itaque velit molestias, eos debitis dignissimos quaerat sed explicabo qui rem.'
+		article: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores perferendis hic deserunt cumque reiciendis distinctio repellat doloremque in, quas, itaque velit molestias, eos debitis dignissimos quaerat sed explicabo qui rem.',
+		time: '12-20-30'
 	}, {
 		id: 2,
 		author: 'Vasya Pupkin',
-		article: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate non omnis facilis, tempora! Doloribus veritatis, suscipit nostrum aperiam minus, provident neque ipsum repellat deserunt totam alias optio blanditiis architecto! Laudantium iste laboriosam repellendus reiciendis commodi illo ullam beatae porro possimus unde nihil, veniam inventore alias, et fugit dolores quas. Reprehenderit id quos sapiente natus dignissimos ab eius aperiam maxime provident, quod laborum esse dolorem possimus repudiandae inventore accusantium iure unde veritatis ut dolore qui minima quasi sint necessitatibus sunt. Ad ut velit adipisci beatae reprehenderit, impedit facilis. Hic quasi quos perspiciatis architecto earum minus et, ea fugit, voluptas, at quas.'
+		article: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate non omnis facilis, tempora! Doloribus veritatis, suscipit nostrum aperiam minus, provident neque ipsum repellat deserunt totam alias optio blanditiis architecto! Laudantium iste laboriosam repellendus reiciendis commodi illo ullam beatae porro possimus unde nihil, veniam inventore alias, et fugit dolores quas. Reprehenderit id quos sapiente natus dignissimos ab eius aperiam maxime provident, quod laborum esse dolorem possimus repudiandae inventore accusantium iure unde veritatis ut dolore qui minima quasi sint necessitatibus sunt. Ad ut velit adipisci beatae reprehenderit, impedit facilis. Hic quasi quos perspiciatis architecto earum minus et, ea fugit, voluptas, at quas.',
+		time: '33-44-55'
 	}
 ];
 
@@ -17,6 +19,7 @@ let Article = React.createClass({
 			<li>
 				<div> {this.props.author} </div>
 				<div> {this.props.article} </div>
+				<div> {this.props.time} </div>
 			</li>
 		);
 	}
@@ -50,7 +53,8 @@ let ArticleList = React.createClass({
 						this.state.displayedContacts.map(function (el){
 							return <Article key={el.id}
 															author={el.author}
-															article={el.article} />;
+															article={el.article}
+															time={el.time} />;
 						})
 					}
 				</ul>
@@ -63,3 +67,4 @@ ReactDOM.render(
 	<ArticleList />,
 	document.querySelector('.content')
 );
+
