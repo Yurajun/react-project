fb8caea192f1317cd94489072a0798c59f6c129a
yurajun <yurajun@mail.ru>
2017-03-21 10:52:06 +0300

    Added objec ARTICLES

---
+++ js/node.js
@@ -0,0 +1,11 @@
+let ARTICLES = [
+	{
+		id: 1,
+		author: 'Yura Jun',
+		article: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores perferendis hic deserunt cumque reiciendis distinctio repellat doloremque in, quas, itaque velit molestias, eos debitis dignissimos quaerat sed explicabo qui rem.'
+	}, {
+		id: 2,
+		author: 'Vasya Pupkin',
+		aritcle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate non omnis facilis, tempora! Doloribus veritatis, suscipit nostrum aperiam minus, provident neque ipsum repellat deserunt totam alias optio blanditiis architecto! Laudantium iste laboriosam repellendus reiciendis commodi illo ullam beatae porro possimus unde nihil, veniam inventore alias, et fugit dolores quas. Reprehenderit id quos sapiente natus dignissimos ab eius aperiam maxime provident, quod laborum esse dolorem possimus repudiandae inventore accusantium iure unde veritatis ut dolore qui minima quasi sint necessitatibus sunt. Ad ut velit adipisci beatae reprehenderit, impedit facilis. Hic quasi quos perspiciatis architecto earum minus et, ea fugit, voluptas, at quas.'
+	}
+];
